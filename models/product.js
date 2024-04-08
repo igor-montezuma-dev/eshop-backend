1e72ba3e7c87962e080e956440b1c1321438d65a
IgorMontezuma20 <igorminranda17@gmail.com>
2024-04-08 10:20:05 -0300

    feat: Create complete Product Schema

--- models/product.js
+++ models/product.js
@@ -1,12 +1,63 @@
 const mongoose = require("mongoose");
+const { Category } = require("./category");
 
 const productSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    countInStock: {
-      type: Number,
-      required: true,
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  richDescription: {
+    type: String,
+    default: "",
+  },
+  image: {
+    type: String,
+    default: "",
+  },
+  images: [
+    {
+      type: String,
     },
-  });
+  ],
+  brand: {
+    type: String,
+    default: "",
+  },
+  price: {
+    type: Number,
+    default: 0,
+  },
+  Category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+    required: true,
+  },
+  countInStock: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 255,
+  },
+  rating: {
+    type: Number,
+    default: 0,
+  },
+  numReviews: {
+    type: Number,
+    default: 0,
+  },
+  isFeatured: {
+    type: Boolean,
+    default: false,
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-  exports.Product = mongoose.model("Product", productSchema);
+exports.Product = mongoose.model("Product", productSchema);
