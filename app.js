a1a20ef13602f4f1948becae37e15171802dc24f
IgorMontezuma20 <igorminranda17@gmail.com>
2024-04-07 21:43:11 -0300

    feat: Creating the api routes and schemas

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-require("dotenv/config");
-const express = require("express");
+const express = require('express');
 const app = express();
-const mongoose = require("mongoose");
-const api = process.env.API_URL;
-const morgan = require("morgan");
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 
+require('dotenv/config');
+
+const api = process.env.API_URL;
+
+const productsRoutes = require('./routers/products');
+const categoriesRoutes = require('./routers/categories');
+const ordersRoutes = require('./routers/orders');
+const usersRoutes = require('./routers/users');
+
+
+//database connection
 mongoose
   .connect(process.env.DATABASE_URL)
   .then(() => {
@@ -18,44 +27,12 @@ mongoose
 app.use(express.json());
 app.use(morgan("tiny"));
 
+//Routers
+app.use(`${api}/products`, productsRoutes);
+app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/orders`, ordersRoutes);
+app.use(`${api}/users`, usersRoutes);
+
 app.listen(3000, () => {
   console.log("Server is running http://localhost:3000");
-});
-
-const productSchema = new mongoose.Schema({
-  name: String,
-  image: String,
-  countInStock: {
-    type: Number,
-    required: true,
-  },
-});
-
-const Product = mongoose.model("Product", productSchema);
-
-app.get(`${api}/products`, async (req, res) => {
-  const productList = await Product.find();
-  if(!productList) {
-    res.status(500).json({success: false})
-  }
-  res.send(productList);
-});
-
-app.post(`${api}/products`, (req, res) => {
-  const product = new Product({
-    name: req.body.name,
-    image: req.body.image,
-    countInStock: req.body.countInStock,
-  });
-  product
-    .save()
-    .then((createdProduct) => {
-      res.status(201).json(createdProduct);
-    })
-    .catch((err) => {
-      res.status(500).json({
-        error: err,
-        success: false,
-      });
-    });
 });
