c4811caef1741df564f36fb64d5899b304193009
IgorMontezuma20 <igorminranda17@gmail.com>
2024-04-12 23:13:42 -0300

    feat: Add update product

--- routers/products.js
+++ routers/products.js
@@ -40,6 +40,34 @@ router.post(`/`, async (req, res) => {
   product = await product.save();
   if (!product) return res.status(500).send("The product cannon be created");
 
+  res.status().send(product, "Product successfully created!");
+});
+
+router.put("/:id", async (req, res) => {
+  const category = await Category.findById(req.body.category);
+  if (!category) return res.status(400).send("Invalid category!");
+
+  const product = await Product.findByIdAndUpdate(
+    req.params.id,
+    {
+      name: req.body.name,
+      description: req.body.description,
+      richDescription: req.body.richDescription,
+      image: req.body.image,
+      brand: req.body.brand,
+      price: req.body.price,
+      category: req.body.category,
+      countInStock: req.body.countInStock,
+      rating: req.body.rating,
+      isFeatured: req.body.isFeatured,
+      dateCreated: req.body.dateCreated,
+    },
+    { new: true }
+  );
+
+  if (!product) {
+    return res.status(500).send("The product cannot be updated!");
+  }
   res.send(product);
 });
 
