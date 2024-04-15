acca975fbc183c18df88e18cc1bbc1bbe94e8898
IgorMontezuma20 <igorminranda17@gmail.com>
2024-04-14 21:48:10 -0300

    feat: Add authorization in api routes

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
-const mongoose = require('mongoose');
+const morgan = require("morgan");
+const mongoose = require("mongoose");
 
-require('dotenv/config');
+require("dotenv/config");
 
 const api = process.env.API_URL;
 
 //cors
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
-app.options('*', cors());
+app.options("*", cors());
 
-const productsRoutes = require('./routers/products');
-const categoriesRoutes = require('./routers/categories');
-const ordersRoutes = require('./routers/orders');
-const usersRoutes = require('./routers/users');
-
+const productsRoutes = require("./routers/products");
+const categoriesRoutes = require("./routers/categories");
+const ordersRoutes = require("./routers/orders");
+const usersRoutes = require("./routers/users");
+const authJwt = require("./helpers/jwt");
 
 //database connection
 mongoose
@@ -31,6 +31,7 @@ mongoose
 //middleware
 app.use(express.json());
 app.use(morgan("tiny"));
+app.use(authJwt());
 
 //Routers
 app.use(`${api}/products`, productsRoutes);
