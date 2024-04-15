03777967609983463a0597b2ada02ead0718c07f
IgorMontezuma20 <igorminranda17@gmail.com>
2024-04-14 22:32:16 -0300

    fix: Excluding login, products and categories routes from the authentication

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,11 +1,20 @@
 const expressJwt = require("express-jwt");
+const { patch } = require("../routers/users");
 
 function authJwt() {
   const secret = process.env.secret;
+  const api = process.env.API_URL;
 
   return expressJwt({
     secret,
     algorithms: ["HS256"],
+  }).unless({
+    path: [
+      `${api}/users/login`,
+      `${api}/users/register`,
+      { url: /\/api\/v1\/products(.*)/, methods: ["GET", "OPTIONS"] },
+      { url: /\/api\/v1\/categories(.*)/, methods: ["GET", "OPTIONS"] },
+    ],
   });
 }
 module.exports = authJwt;
