87bc47a2c0ab65baea645cbf74788306d8548e3a
IgorMontezuma20 <igorminranda17@gmail.com>
2024-04-20 20:52:07 -0300

    feat: Add calculate total price method

--- routers/orders.js
+++ routers/orders.js
@@ -41,6 +41,21 @@ router.post(`/`, async (req, res) => {
   );
   const orderIdResolved = await orderItemsIds;
 
+  const totalPrices = await Promise.all(
+    orderIdResolved.map(async (orderItemId) => {
+      const orderItem = await OrderItem.findById(orderItemId).populate(
+        "product",
+        "price"
+      );
+      const totalPrice = orderItem.product.price * orderItem.quantity;
+      return totalPrice;
+    })
+  );
+
+  const totalPrice = totalPrices.reduce((a, b) => a + b, 0);
+
+  console.log(totalPrice);
+
   let order = new Order({
     orderItems: orderIdResolved,
     shippingAddress1: req.body.shippingAddress1,
@@ -50,7 +65,7 @@ router.post(`/`, async (req, res) => {
     country: req.body.country,
     phone: req.body.phone,
     status: req.body.status,
-    totalPrice: req.body.totalPrice,
+    totalPrice: totalPrice,
     user: req.body.user,
   });
 
