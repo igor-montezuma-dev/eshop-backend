b5ee82cfbe8ed3bcdbea155804eb8775b66f640d
IgorMontezuma20 <igorminranda17@gmail.com>
2024-04-20 21:05:55 -0300

    feat: Add total orders and sales routes

--- routers/orders.js
+++ routers/orders.js
@@ -26,6 +26,29 @@ router.get(`/`, async (req, res) => {
   res.send(orderList);
 });
 
+router.get("/get/totalsales", async (req, res) => {
+  const totalSales = await Order.aggregate([
+    { $group: { _id: null, totalsales: { $sum: "$totalPrice" } } },
+  ]);
+
+  if (!totalSales) {
+    return res.status(400).send("The order sales cannot be generated!");
+  } else {
+    res.send({ totalsales: totalSales.pop().totalsales });
+  }
+});
+
+router.get("/get/totalorders", async (req, res) => {
+  const orderCount = await Order.countDocuments();
+
+  if (!orderCount) {
+    res.status(500).json({ success: false });
+  }
+  res.send({
+    orderCount: orderCount,
+  });
+});
+
 router.post(`/`, async (req, res) => {
   const orderItemsIds = Promise.all(
     req.body.orderItems.map(async (orderItem) => {
