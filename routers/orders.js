e6e5e39c843eb03c841f2e3123b95a1112bbb772
IgorMontezuma20 <igorminranda17@gmail.com>
2024-04-20 21:14:13 -0300

    feat: Add user orders route

--- routers/orders.js
+++ routers/orders.js
@@ -26,6 +26,19 @@ router.get(`/`, async (req, res) => {
   res.send(orderList);
 });
 
+router.get(`/get/userorders/:userid`, async (req, res) => {
+  const userOrderList = await Order.find({ user: req.params.userid })
+    .populate({
+      path: "orderItems",
+      populate: { path: "product", populate: "category" },
+    })
+    .sort({ dateOrdered: -1 });
+  if (!userOrderList) {
+    res.status(500).json({ success: false });
+  }
+  res.send(userOrderList);
+});
+
 router.get("/get/totalsales", async (req, res) => {
   const totalSales = await Order.aggregate([
     { $group: { _id: null, totalsales: { $sum: "$totalPrice" } } },
