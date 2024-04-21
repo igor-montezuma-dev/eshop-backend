a7abd7b56a02048831c3f57560c79dee6c8a02d7
IgorMontezuma20 <igorminranda17@gmail.com>
2024-04-21 16:29:27 -0300

    documentation

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
 
 require("dotenv/config");
 
@@ -16,6 +18,7 @@ const productsRoutes = require("./routers/products");
 const categoriesRoutes = require("./routers/categories");
 const ordersRoutes = require("./routers/orders");
 const usersRoutes = require("./routers/users");
+const swaggerDocument = require("./swagger.json");
 const authJwt = require("./helpers/jwt");
 const errorHandler = require("./helpers/error-handler");
 
@@ -41,6 +44,10 @@ app.use(`${api}/products`, productsRoutes);
 app.use(`${api}/categories`, categoriesRoutes);
 app.use(`${api}/orders`, ordersRoutes);
 app.use(`${api}/users`, usersRoutes);
+
+//swagger
+
+app.use(`${api}/api-docs`, swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.listen(3000, () => {
   console.log("Server is running http://localhost:3000");
--- helpers/jwt.js
+++ helpers/jwt.js
@@ -9,11 +9,12 @@ function authJwt() {
     isRevoked: isRevoked,
   }).unless({
     path: [
-       { url: /\/public\/uploads(.*)/, methods: ['GET', 'OPTIONS'] },
-       { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
-       { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
-       `${api}/users/login`,
-       `${api}/users/register`,
+      { url: /\/public\/uploads(.*)/, methods: ["GET", "OPTIONS"] },
+      { url: /\/api\/v1\/products(.*)/, methods: ["GET", "OPTIONS"] },
+      { url: /\/api\/v1\/categories(.*)/, methods: ["GET", "OPTIONS"] },
+      { url: /\/api\/v1\/api-docs(.*)/, methods: ["GET", "OPTIONS"] },
+      `${api}/users/login`,
+      `${api}/users/register`,
       //{ url: /(.*)/ },
     ],
   });
